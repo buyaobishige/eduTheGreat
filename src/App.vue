a0097e7c30cf4c24f465d01191242a08546be821
buyaobishige <1105936025@163.com>
2019-10-22 22:14:57 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <div id="app">
+  <div :class="nightmode == true ? 'night' : 'daylight'" id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TableDemo />
+
+    <button @click="nightmode = !nightmode">NIGHT</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import TableDemo from "./components/TableDemo.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    TableDemo
+  },
+  data: () => {
+    return {
+      nightmode: false
+    };
   }
 };
 </script>
@@ -24,5 +34,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.night {
+  background: #000;
 }
 </style>
---
+++ src/components/TableDemo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- <table border="1">
+      <tr :key="i" v-for="i in 5">
+        <td :key="j" v-for="j in 6">OK</td>
+      </tr>
+    </table>-->
+    <div id="gitalk-container"></div>
+    <h1>hello world</h1>
+  </div>
+</template>
+
+<script>
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+
+export default {
+  name: "TableDemo",
+  mounted() {
+    const gitalk = new Gitalk({
+      clientID: "f0e39100fb4cad63b187",
+      clientSecret: "21da90ff8a967dba2ee52c9a323ff7feddbfbf39",
+      repo: "eduTheGreat",
+      owner: "buyaobishige",
+      admin: ["buyaobishige"],
+      id: location.pathname, // Ensure uniqueness and length less than 50
+      distractionFreeMode: false, // ,Facebook-like distraction free mode
+      number: 1
+    });
+    gitalk.render("gitalk-container");
+  }
+};
+</script>
+<style lang="stylus" scoped>
+/* table
+  for i in 0 1 2 3
+    tr:nth-child({i})
+      $set = red blue blue green
+      background ($set[i]) */
+</style>
